0db4f641b8c434437b0604f14bf427b8545e48c2
Kamran Azim <Kamazim121212@gmail.com>
2024-04-03 02:38:35 +0100

    Check to see if time is 9am before sending

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import smtplib
+import datetime as dt
 from random import randint
+from random import choice
 
 MY_EMAIL = "lambdaa112@gmail.com"
 TARGET_EMAIL = "kamazim121212@gmail.com"
 PASSWORD = "ynha ervn pgwy cink"
+names = ["Jessie", "James"]
 
 # Can use similar to file context manager
-with smtplib.SMTP("smtp.gmail.com") as connection: # A way to connect to our email providers smtp email server
-    
-    # Grabbing Quote
-    with open("quotes.txt", "r") as f:
-        lines = f.readlines()
-        random_num = randint(0,len(lines) - 1)
-        quote = lines[random_num].strip()
-        del lines[random_num] 
+while True:
 
-    connection.starttls() # Stands for transport layer security and is a way of securing our connection to an email server
-    connection.login(user=MY_EMAIL, password = PASSWORD) 
-    connection.sendmail(from_addr=MY_EMAIL, to_addrs=TARGET_EMAIL, msg = f"Subject:Motivational Quote\n\n{quote}\n\nAutomated with Python") 
-    # To add subject use colon and then two newlines for content
+    # Checking Date and Time
+    if dt.datetime.now().minute == 36:
+        
+        # Grabbing Quote
+        with open("quotes.txt", "r") as f:
+            lines = f.readlines()
+            random_num = randint(0,len(lines) - 1)
+            quote = lines[random_num].strip()
+            del lines[random_num] 
+
+        with smtplib.SMTP("smtp.gmail.com") as connection: # A way to connect to our email providers smtp email server
+            # Sending Email
+            connection.starttls() # Stands for transport layer security and is a way of securing our connection to an email server
+            connection.login(user=MY_EMAIL, password = PASSWORD) 
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=TARGET_EMAIL, msg = f"Subject:Motivational Quote\n\n{quote}\n\n{choice(names)} from Team Rocket") 
+            # To add subject use colon and then two newlines for content
+            break
